dbf8948a791139b1b2ed58250f95ed811a7a6290
Urojqui <unairojasquiros@gmail.com>
2024-10-25 18:46:50 +0200

    home update

--- pages/Home/home.js
+++ pages/Home/home.js
@@ -7,9 +7,11 @@ export const Home = () => {
   main.innerHTML = `
   <section class="background"></section>
     <div class="home">
+    <div class="glass">
     <p class="hello">Hey, I'm</p>
     <h1>Unai Rojas</h1>
     <p>Born in Valencia, living in Seville, Spain. I'm studying the PROMETEO program at THE POWER SCHOOL that means I'm a Web Applications Development and Full Stack Development student. Here you can take a look of the skills I'm learning. I hope you like it. </p>
     <a href="mailto:unairojasquiros@gmail.com">Contact →</a>
+    </div>
     </div>`;
 };
